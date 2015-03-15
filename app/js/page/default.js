bfa46045da2c20aa4f58fcafd970006b7dad2260
Paul Swartz <pswartz@zipcar.com>
2015-03-15 17:31:44 -0400

    initial commit

---
+++ app/js/page/default.js
@@ -0,0 +1,25 @@
+define(function (require) {
+
+  'use strict';
+
+  /**
+   * Module dependencies
+   */
+
+  // var MyComponent = require('component/my_component');
+
+  /**
+   * Module exports
+   */
+
+  return initialize;
+
+  /**
+   * Module function
+   */
+
+  function initialize() {
+    // MyComponent.attachTo(document);
+  }
+
+});
