08f16f14a122e9860a0598952e0f6d56958db987
Paul Swartz <pswartz@zipcar.com>
2015-03-15 19:44:10 -0400

    mixin for displaying data in a simple table

---
+++ app/js/component/with_render_table.js
@@ -0,0 +1,66 @@
+define(function (require) {
+
+  'use strict';
+
+  /**
+   * Module exports
+   */
+
+  return withRenderTable;
+
+  /**
+   * Module function
+   */
+
+  function withRenderTable() {
+    this.attributes({
+      tableSelector: 'table',
+      fields: [],
+      formatters: {}
+    });
+
+    this.formatField = function formatField(field, value) {
+      if (this.attr.formatters[field]) {
+        value = this.attr.formatters[field](value);
+      }
+      return value;
+    };
+
+    this.render = function render() {
+      var $table = this.select('tableSelector');
+      $table.html(this.content());
+    };
+
+    this.content = function content() {
+      var output = [],
+          columns = [];
+      function appendRow(columns) {
+        output.push('<tr>' + columns.join('') + '</tr>');
+      }
+      this.attr.fields.forEach(function(field) {
+        columns.push('<th>' + field.display + '</th>');
+      });
+      appendRow(columns);
+      this.data.forEach(function(row) {
+        columns = [];
+        this.attr.fields.forEach(function(field) {
+          var value = row[field.name];
+          columns.push(
+            '<td>' + this.formatField(field.name, value) + '</td>');
+        }, this);
+        appendRow(columns);
+      }, this);
+      return output;
+    };
+
+    this.setData = function setData(data) {
+      this.data = data;
+      this.render();
+    };
+
+    this.after('initialize', function () {
+      this.setData([]);
+    });
+  }
+
+});
---
+++ test/spec/component/with_render_table.spec.js
@@ -0,0 +1,62 @@
+'use strict';
+
+describeMixin('component/with_render_table', function () {
+
+  // Initialize the component and attach it to the DOM
+  beforeEach(function () {
+    setupComponent('<div><table></table></div>', {
+      tableSelector: 'table',
+      fields: [{
+        name: 'name',
+        display: 'Display'
+      }, {
+        name: 'date',
+        display: 'Date'
+      }],
+      formatters: {
+        date: function(value) {
+          return 'formatted value';
+        }
+      }
+    });
+  });
+
+  it('should be defined', function () {
+    expect(this.component).toBeDefined();
+  });
+
+  it('should render the headers', function() {
+    var $headers = this.$node.find('th'),
+        headerDisplays = $headers.map(function(index, el) {
+          return el.innerText;
+        }).toArray();
+
+    expect(headerDisplays).toEqual(['Display', 'Date']);
+  });
+
+  it('should render data/formatted data', function() {
+    this.component.setData([{
+      name: 'name value',
+      date: 'date value'
+    }]);
+    var $values = this.$node.find('td'),
+        valueDisplays = $values.map(function(index, el) {
+          return el.innerText;
+        }).toArray();
+
+    expect(valueDisplays).toEqual(['name value', 'formatted value']);
+  });
+
+  it('can render multiple sets of data', function() {
+    this.component.setData([{
+      name: 'name value',
+      date: 'date value'
+    }, {
+      name: 'name 2',
+      date: 'date 2'
+    }]);
+
+    expect(this.$node.find('tr').length).toEqual(3); // header + 2 rows
+    expect(this.$node.find('td').length).toEqual(4); // 2 rows * 2 values
+  });
+});
